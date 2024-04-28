bfc7ab110679774b395d905c55f0b9f961da4e5e
Praveen R <136920752+gitzpraveen@users.noreply.github.com>
2024-04-28 18:34:23 +0530

    div fun is added

--- .github/workflows/New.java
+++ .github/workflows/New.java
@@ -1,11 +1,11 @@
 public class New {
 
     public static void main(String[] args) throws Exception{
-    int i=1;
-        while(true)
-            {
-                i++;
-            }
-                
+
+           public void div()
+    {
+        System.out.print("div is added");
+    }
+        
     }
 }
