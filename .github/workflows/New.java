00c5e855fef1876a8ab6cc61b11034d21a8a0cbf
Praveen R <136920752+gitzpraveen@users.noreply.github.com>
2024-04-28 18:09:17 +0530

    while loop is added

--- .github/workflows/New.java
+++ .github/workflows/New.java
@@ -1,9 +1,11 @@
 public class New {
 
     public static void main(String[] args) throws Exception{
-
-        System.out.println("hello world in chennai");
-         System.out.println("hello world in bangalore ");
-        
+    int i=1;
+        while(true)
+            {
+                i++;
+            }
+                
     }
 }
