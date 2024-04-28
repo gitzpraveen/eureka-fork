f523ab99f5574f587e130d3b56c55cc52cd5e029
Praveen R <136920752+gitzpraveen@users.noreply.github.com>
2024-04-28 18:00:31 +0530

    bangalore sop added

--- .github/workflows/New.java
+++ .github/workflows/New.java
@@ -2,6 +2,8 @@ public class New {
 
     public static void main(String[] args) throws Exception{
 
-        System.out.println("hello world");
+        System.out.println("hello world in chennai");
+         System.out.println("hello world in bangalore ");
+        
     }
 }
